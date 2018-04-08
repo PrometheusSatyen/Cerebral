2d9f0e1ac7d9d5e77de7ae18a8a9ee3fb4100e3d
Prometheus Satyen <prometheussatyen@gmail.com>
2018-04-09 00:07:51 +0100

    Removal manual refresh button

--- src/components/views/Overview.jsx
+++ src/components/views/Overview.jsx
@@ -3,14 +3,13 @@
 import React from 'react';
 
 import AddCharacterButton from '../buttons/AddCharacterButton';
-import RefreshButton from '../buttons/RefreshButton';
 import CharactersOverviewTable from '../tables/CharactersOverviewTable';
 
 export default class Overview extends React.Component {
     render() {
         return (
             <div>
-                <AddCharacterButton/> <RefreshButton/>
+                <AddCharacterButton/>
                 <CharactersOverviewTable/>
             </div>
         );
