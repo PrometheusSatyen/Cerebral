76c61a3bca566e0bae7a07496e7d556e85755af5
Harry Reeder <harry@harryreeder.co.uk>
2018-05-26 11:43:11 +0100

    Fix alpha detection - should be if trained > active

--- src/models/Character.js
+++ src/models/Character.js
@@ -177,8 +177,8 @@ class Character {
             return true;
         }
 
-        // if they have any skills with active level > enabled, must be alpha
-        if (this.skills.find(o => o.active_skill_level > o.trained_skill_level) !== undefined) {
+        // if they have any skills with trained level > active, must be alpha
+        if (this.skills.find(o => o.trained_skill_level > o.active_skill_level) !== undefined) {
             return false;
         }
 
